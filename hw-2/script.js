fb186b137584180f185fc8bc693cd1de68e087cb
vadyanator <vadok.hohol1999@gmail.com>
2021-11-08 10:27:26 +0200

    final hw2

--- hw-2/script.js
+++ hw-2/script.js
@@ -1,26 +1,37 @@
-const FIRST_NUMBER = 0;
-const SECOND_NUMBER = 1;
-let serialNumber;
-let isNumberCorrect;
+const FIRST_NUMBER = 0,
+  SECOND_NUMBER = 1;
+
+let serialNumber, isNumberCorrect, isUserConfirm;
 
 do {
   isNumberCorrect = true;
-  serialNumber = +prompt("Enter serial number of fibonacci row");
+  isUserConfirm = true;
+  serialNumber = prompt("Enter serial number of fibonacci row");
 
-  if (isNaN(serialNumber) || Math.trunc(serialNumber) !== serialNumber) {
+  if (
+    isNaN(+serialNumber) ||
+    Math.trunc(+serialNumber) !== +serialNumber ||
+    serialNumber === ""
+  ) {
     alert("Please, enter an integer");
     isNumberCorrect = false;
   }
+
+  if (!serialNumber) {
+    isUserConfirm = false;
+  }
 } while (!isNumberCorrect);
 
-const specifiedFibonacciNumber = getSpecifiedFibonacciNumber(
-  FIRST_NUMBER,
-  SECOND_NUMBER,
-  serialNumber
-);
+if (isUserConfirm) {
+  const specifiedFibonacciNumber = getSpecifiedFibonacciNumber(
+    FIRST_NUMBER,
+    SECOND_NUMBER,
+    +serialNumber
+  );
 
-const formattedSerialNumber = formatNumberToSerial(serialNumber);
+  const formattedSerialNumber = formatNumberToSerialString(serialNumber);
 
-alert(
-  `The ${formattedSerialNumber} number of fibonacci row is ${specifiedFibonacciNumber}`
-);
+  alert(
+    `The ${formattedSerialNumber} number of fibonacci row is ${specifiedFibonacciNumber}`
+  );
+}
